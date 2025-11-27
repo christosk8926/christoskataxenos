c9970b588509c772eda94953f268a9e9e9edfed9
Christos Kataxenos <christosk8926@gmail.com>
2025-11-27 17:54:31 +0100

    Remove metadata from BlogPage component
    Removed metadata export for the blog page.

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -3,9 +3,7 @@ import UnderConstruction from '../../components/UnderConstruction';
 import { CodeIcon } from '../../components/Icons';
 import SpotlightCard from '../../components/SpotlightCard';
 
-export const metadata = {
-  title: "Dev Blog | Christos Kataxenos",
-};
+
 
 export default function BlogPage() {
   return <UnderConstruction title="Dev Blog" Icon={CodeIcon} SpotlightCard={SpotlightCard} />;
