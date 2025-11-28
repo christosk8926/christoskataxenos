7f9934fdf2eb58dd397dfea04476e465df508d91
Christos Kataxenos <christosk8926@gmail.com>
2025-11-28 19:06:37 +0100

    fix(build): Remove deleted MouseProvider from Providers.js

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { MouseProvider } from '../context/MouseContext';
 import { LanguageProvider } from '../context/LanguageContext';
 import ScrollProgressIndicator from './ScrollProgressIndicator';
 
 export function Providers({ children }) {
   return (
     <LanguageProvider>
-      <MouseProvider>
-        <ScrollProgressIndicator />
-        {children}
-      </MouseProvider>
+      <ScrollProgressIndicator />
+      {children}
     </LanguageProvider>
   );
 }
